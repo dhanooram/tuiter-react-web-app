e9756ecf576a3d423f664ef33ca0b1617806022d
dhanooramnagaraj <dhanoonickson@gmail.com>
2022-10-16 13:47:54 -0400

    code update -msg change

--- public/Tuiter/HomeScreen/posts.js
+++ public/Tuiter/HomeScreen/posts.js
@@ -19,8 +19,6 @@ export default  [
     time: '2h',
     titleDescription: ' It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
     image: 'https://img.andrewprokos.com/MIDTOWN-MANHATTAN-SKYLINE-FROM-WEEHAWKEN-NIGHT-PANORAMIC-3931-1200PX.jpg',
-    mgHeading:'1914 translation by H. Rackham',
-    mageDescription: 'On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue.',
     imgHeading:'',
     imageDescription:'',
     commentCount:'22',
